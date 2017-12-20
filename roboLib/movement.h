b26c0ca905d38db3126805fffa98507cbece6a04
ttaloub <34716800+ttaloub@users.noreply.github.com>
2017-12-20 17:46:30 +0100

    Accents retirés

--- roboLib/movement.h
+++ roboLib/movement.h
@@ -4,51 +4,51 @@
 #include "config.h"
 #include "position.h"
 
-/* Bibliothèque de mouvement haut-niveau et bas niveau.
- * La partie bas niveau effectue des déplacements en s'arrêtant en cas d'obstacles, et gère l'asservissement des moteurs.
- * La partie haut niveau permet au robot d'aller d'un point A à un point B en gérant les obstacles de façon optimale.
+/* Bibliotheque de mouvement haut-niveau et bas niveau.
+ * La partie bas niveau effectue des deplacements en s'arretant en cas d'obstacles, et gere l'asservissement des moteurs.
+ * La partie haut niveau permet au robot d'aller d'un point A a un point B en gerant les obstacles de faÃ§on optimale.
  */
 
 //Partie haut niveau
 
 const float timeout = 10000;
 
-/* moveTo() trouve les mouvements individuels optimaux pour aller jusqu'à la destination.
- * les obstacles sont gérés (et le mouvement retenté) tant que le mouvement n'a pas excédé la valeur de timeout, auquel cas false sera renvoyé.
- * si ignoreOrientation est vrai, le robot ne cherchera pas à se mettre dans l'axe spécifié dans target.
+/* moveTo() trouve les mouvements individuels optimaux pour aller jusqu'a la destination.
+ * les obstacles sont geres (et le mouvement retente) tant que le mouvement n'a pas excede la valeur de timeout, auquel cas false sera renvoye.
+ * si ignoreOrientation est vrai, le robot ne cherchera pas a se mettre dans l'axe specifie dans target.
  */
 bool moveTo(struct PosData const* target, bool ignoreOrientation = false);
 
-//moveTo préfère utiliser arc() pour économiser des mouvements ; ici, des straight() seront utilisés.
+//moveTo prÃ©fÃ¨re utiliser arc() pour economiser des mouvements ; ici, des straight() seront utilises.
 bool moveTo_forceStraight(struct PosData const* target, bool ignoreOrientation = false);
 
 //Partie bas-niveau
 
-/* MovementStates recense les différents états possibles d'un mouvement :
- * TBD : mouvement à faire, non commencé ;
- * ONGOING : mouvement en cours d'exécution ;
- * DONE : mouvement correctement effectué ;
- * FAILED_OBS : mouvement échoué du fait d'un obstacle rencontré ;
- * FAILED_OTHER : mouvement échoué pour une autre raison (batterie faible, etc.).
- * Les états TBD et ONGOING ne devraient normalement pas être observés, mais peuvent être utiles pour du débuggage.
+/* MovementStates recense les differents etats possibles d'un mouvement :
+ * TBD : mouvement a faire, non commence ;
+ * ONGOING : mouvement en cours d'execution ;
+ * DONE : mouvement correctement effectue ;
+ * FAILED_OBS : mouvement echoue du fait d'un obstacle rencontre ;
+ * FAILED_OTHER : mouvement echoue pour une autre raison (batterie faible, etc.).
+ * Les etats TBD et ONGOING ne devraient normalement pas etre observes, mais peuvent etre utiles pour du debuggage.
  */
 enum MovementState {
 	TBD, ONGOING, DONE, FAILED_OBS, FAILED_OTHER
 };
 
-struct MovementResult { //Mettre dans cette structure toutes les données qu'un mouvement doit renvoyer (pour l'instant, juste state)
+struct MovementResult { //Mettre dans cette structure toutes les donnees qu'un mouvement doit renvoyer (pour l'instant, juste state)
 	enum MovementState state;
 };
 
 /* Trois fonctions de mouvement principales : straight, arc et rotate.
- * Elles modifient MovementResult qui contient les données du déplacement.
- * Il faudra éventuellement relancer un mouvement à la suite de l'exécution d'une fonction de mouvement (si il ne s'est pas effectué correctement).
- * On s'appuiera alors sur getPosition et sur des données externes d'emplacement de la cible.
+ * Elles modifient MovementResult qui contient les donnees du dÃ©placement.
+ * Il faudra eventuellement relancer un mouvement a la suite de l'execution d'une fonction de mouvement (s'il ne s'est pas effectue correctement).
+ * On s'appuiera alors sur getPosition et sur des donnees externes d'emplacement de la cible.
  */
 
 void straight(float distance);
 
-void arc(float distance, float angle); //La distance est la distance entre le point de départ et le point d'arrivée (pas la distance parcourure).
+void arc(float distance, float angle); //La distance est la distance entre le point de depart et le point d'arrivee (pas la distance parcourue).
 
 void rotate(float angle);
 
@@ -60,14 +60,14 @@ void getMovementResult(struct MovementResult const** r);
 // ---------- CONSTANTES A MODIFIER ----------
 
 /* minSpeed : vitesse minimale pour laquelle laquelle les moteurs bougent.
- * maxAcceleration : accélération maximale permettant d'éviter les glissements.
+ * maxAcceleration : accelÃ©ration maximale permettant d'eviter les glissements.
  * maxSpeed : vitesse plafond du robot.
- * securityDistance : distance à un obstacle que le robot essaiera de respecter.
- * Une distance de sécurité trop faible fera éventuellement dépasser maxAcceleration.
- * errorMarginDistance : marge d'erreur acceptable en distance à une cible à la fin du mouvement.
- * errorMarginAngle : marge d'erreur acceptable en angle à une cible à la fin du mouvement.
+ * securityDistance : distance a un obstacle que le robot essaiera de respecter.
+ * Une distance de securite trop faible fera eventuellement depasser maxAcceleration.
+ * errorMarginDistance : marge d'erreur acceptable en distance a une cible a la fin du mouvement.
+ * errorMarginAngle : marge d'erreur acceptable en angle a une cible a la fin du mouvement.
  *
- * Distances en mm, temps en s, angles en degrés.
+ * Distances en mm, temps en s, angles en degres.
  */
 
 const float minSpeed = 50;
@@ -77,31 +77,31 @@ const float securityDistance = 50;
 const float errorMarginDistance = 10;
 const float errorMarginAngle = 2;
 
-//Angle maximal pour lequel le robot choisit d'effectuer un arc de cercle plutôt que de tourner sur soi-même avant, en degrés, durant l'exécution de moveTo().
+//Angle maximal pour lequel le robot choisit d'effectuer un arc de cercle plutot que de tourner sur soi-meme avant, en degres, durant l'execution de moveTo().
 const float maxAngleForArc = 35;
 
 // ---------- FIN DES CONSTANTES A MODIFIER ----------
 
 struct MovementResult __result;
 
-//TODO : gérer les échecs et le timeout.
+//TODO : gerer les echecs et le timeout.
 bool moveTo(struct PosData const* target, bool ignoreOrientation) {
 	struct PosData const* pos = NULL;
 	getPosition(&pos);
 
 	float targetDirection = atan((target->y - pos->y) / (target->x - pos->x)) * 180 / PI;
-	float deltaOToFace = targetDirection - pos->orientation; //Différence entre l'orientation que le robot aurait si dirigé vers la cible et son orientation actuelle.
+	float deltaOToFace = targetDirection - pos->orientation; //Difference entre l'orientation que le robot aurait si dirige vers la cible et son orientation actuelle.
 	float deltaD = sqrt(pow(target->x - pos->x, 2) + pow(target->y - pos->y, 2));
 
-	if (deltaOToFace < maxAngleForArc) { //On économise une rotation avant le mouvement vers la cible.
+	if (deltaOToFace < maxAngleForArc) { //On economise une rotation avant le mouvement vers la cible.
 		arc(deltaD, deltaOToFace);
 		getPosition(&pos);
 		if (!ignoreOrientation)
 			rotate(target->orientation - pos->orientation);
 	}
-	else if (!ignoreOrientation && target->orientation - targetDirection < maxAngleForArc) { //On économise une rotation après le mouvement vers la cible.
+	else if (!ignoreOrientation && target->orientation - targetDirection < maxAngleForArc) { //On economise une rotation apres le mouvement vers la cible.
 		rotate(target->orientation + 2 * (targetDirection - target->orientation));
-		getPosition(&pos); //On actualise la position et les deltas, même si deltaD ne devrait pas avoir changé, au cas où.
+		getPosition(&pos); //On actualise la position et les deltas, meme si deltaD ne devrait pas avoir change, au cas ou.
 		deltaOToFace = targetDirection - pos->orientation;
 		deltaD = sqrt(pow(target->x - pos->x, 2) + pow(target->y - pos->y, 2));
 		arc(deltaD, deltaOToFace);
@@ -111,7 +111,7 @@ bool moveTo(struct PosData const* target, bool ignoreOrientation) {
 		getPosition(&pos);
 		deltaOToFace = targetDirection - pos->orientation;
 		deltaD = sqrt(pow(target->x - pos->x, 2) + pow(target->y - pos->y, 2));
-		arc(deltaD, deltaOToFace); //On effectue tout de même un arc, au cas où.
+		arc(deltaD, deltaOToFace); //On effectue tout de meme un arc, au cas ou.
 		if (!ignoreOrientation) {
 			getPosition(&pos);
 			rotate(target->orientation - pos->orientation);
